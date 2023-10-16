4271cf0f26cdd5ac892598c3c470166ee3531d98
bryjen <miguelbryancarlo3434@gmail.com>
2023-10-16 16:35:52 -0400

    Cleaned up driver code, Ensured code builds properly

--- src/OrdersDriver.cpp
+++ src/OrdersDriver.cpp
@@ -5,19 +5,19 @@
 
 void testOrdersLists()
 {
-    Player *player1 = new Player();
-    Player *player2 = new Player();
+    auto *player1 = new Player();
+    auto *player2 = new Player();
     player1->handCard.push_back(new Card(type::blockade));
     player1->handCard.push_back(new Card(type::bomb));
     player1->handCard.push_back(new Card(type::airlift));
     player1->handCard.push_back(new Card(type::diplomacy));
 
-    Continent *europe = new Continent("Europe", 100);
+    auto *europe = new Continent("Europe", 100);
 
-    Territory *france = new Territory("France", 5, 5, europe, player1, 5);
-    Territory *belgium = new Territory("Belgium", 6, 4, europe, player1, 5);
-    Territory *england = new Territory("England", 5, 3, europe, player2, 5);
-    Territory *greece = new Territory("Greece", 10, 12, europe, player1, 5);
+    auto *france = new Territory("France", 5, 5, europe, player1, 5);
+    auto *belgium = new Territory("Belgium", 6, 4, europe, player1, 5);
+    auto *england = new Territory("England", 5, 3, europe, player2, 5);
+    auto *greece = new Territory("Greece", 10, 12, europe, player1, 5);
 
     france->addAdjacentTerritory(belgium);
     france->addAdjacentTerritory(england);
@@ -26,20 +26,20 @@ void testOrdersLists()
     england->addAdjacentTerritory(france);
     england->addAdjacentTerritory(belgium);
 
-    player1->issueOrder("deploy", france, 2, NULL, nullptr);
+    player1->issueOrder("deploy", france, 2, nullptr, nullptr);
     player1->issueOrder("advance", belgium, 4, france, nullptr);
-    player1->issueOrder("bomb", england, 0, NULL, nullptr);
-    player1->issueOrder("blockade", france, 0, NULL, nullptr);
+    player1->issueOrder("bomb", england, 0, nullptr, nullptr);
+    player1->issueOrder("blockade", france, 0, nullptr, nullptr);
     player1->issueOrder("airlift", england, 3, france, nullptr);
-    player1->issueOrder("negotiate", NULL, 0, NULL, nullptr);
+    player1->issueOrder("negotiate", nullptr, 0, nullptr, nullptr);
 
-    player2->issueOrder("deploy", france, 0, NULL, nullptr);
-    player2->issueOrder("invalid type", NULL, 0, NULL, nullptr);
+    player2->issueOrder("deploy", france, 0, nullptr, nullptr);
+    player2->issueOrder("invalid type", nullptr, 0, nullptr, nullptr);
     player2->issueOrder("advance", belgium, 4, france, nullptr);
-    player2->issueOrder("bomb", england, 0, NULL, nullptr);
-    player2->issueOrder("blockade", france, 0, NULL, nullptr);
+    player2->issueOrder("bomb", england, 0, nullptr, nullptr);
+    player2->issueOrder("blockade", france, 0, nullptr, nullptr);
     player2->issueOrder("airlift", england, 3, france, nullptr);
-    player2->issueOrder("negotiate", NULL, 0, NULL, nullptr);
+    player2->issueOrder("negotiate", nullptr, 0, nullptr, nullptr);
     cout << "ORDERS PART\n-------------------------------------------------------------\n";
 
 
